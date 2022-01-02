4200fa2f0ab49f3920e58374a1f464291555e9ef
Bhutato <anasahad@MacBook-Air.local>
2022-01-03 00:37:42 +0530

    fix frontend issues

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/journalDB");
 
 const homeStartingContent =
-  "All your posts appear here";
+  "All your posts appear here.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
